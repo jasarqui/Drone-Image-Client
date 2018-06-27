ae596c83ab325ecdb4d110bcc2e7b3c9f34b554e
jasarqui <jzarquilita@up.edu.ph>
2018-06-27 10:12:56 +0800

    Revised Analysis Panel and added Google APIs

--- src/components/analysis/Analyze.js
+++ src/components/analysis/Analyze.js
@@ -13,11 +13,14 @@ import {
   Icon,
   Image,
   CardImage,
-  Panel,
-  PanelBlock,
-  PanelIcon,
-  PanelHeading,
-  Button
+  Message,
+  MessageHeader,
+  MessageBody,
+  Button,
+  Menu,
+  MenuLabel,
+  MenuLink,
+  MenuList
 } from 'bloomer';
 
 /* create styles here */
@@ -25,6 +28,15 @@ const style = {
   marginCard: {
     margin: '30px',
     borderRadius: '25px'
+  },
+  icon: {
+    marginRight: '5px'
+  },
+  removeUnderline: {
+    textDecoration: 'none'
+  },
+  marginPanel: {
+    margin: '30px 20px 20px 0px'
   }
 };
 
@@ -32,8 +44,34 @@ export default class Analyze extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      selectedFile: null,
+      metadata: {
+        name: 'sample_file.png',
+        date: '06/27/2018'
+      },
+      attrib: [
+        {
+          name: 'Height',
+          value: '3ft'
+        },
+        {
+          name: 'Width',
+          value: '1in'
+        }
+      ]
+    };
   }
+
+  uploadFile = e => {
+    e.preventDefault();
+    this.setState({ selectedFile: e.target.value });
+  };
+
+  analyzeImage = e => {
+    /* this is where we put the glue */
+    e.preventDefault();
+  };
 
   render() {
     return (
@@ -50,38 +88,83 @@ export default class Analyze extends Component {
               <CardImage>
                 <Image
                   isRatio="4:3"
-                  src="https://via.placeholder.com/1280x960"
+                  src={
+                    this.state.selectedFile
+                      ? this.state.selectedFile
+                      : 'http://www.outokummunseutu.fi/components/com_easyblog/themes/wireframe/images/placeholder-image.png'
+                  }
                 />
               </CardImage>
-              <CardContent>Hello</CardContent>
+              <CardContent>
+                <Columns>
+                  <Column isSize="1/2">
+                    <Button
+                      isFullWidth
+                      isColor="primary"
+                      onClick={this.uploadFile}>
+                      <Icon className="fa fa-upload" style={style.icon} />
+                      Upload Image
+                    </Button>
+                  </Column>
+                  <Column isSize="1/2">
+                    <Button
+                      isFullWidth
+                      isColor="primary"
+                      onClick={this.analyzeImage}>
+                      <Icon className="fa fa-bolt" style={style.icon} />
+                      Analyze
+                    </Button>
+                  </Column>
+                </Columns>
+              </CardContent>
             </Card>
           </Column>
           <Column isSize="1/3">
-            <Panel>
-              <PanelHeading>Lorem Ipsum</PanelHeading>
-              <PanelBlock>
-                <PanelIcon className="fa fa-book" />
-                Bloomer
-              </PanelBlock>
-              <PanelBlock>
-                <PanelIcon className="fa fa-code-fork" />
-                RxJS
-              </PanelBlock>
-              <PanelBlock>
-                <PanelIcon className="fa fa-code-fork" />
-                Webpack
-              </PanelBlock>
-              <PanelBlock>
-                <PanelIcon className="fa fa-code-fork" />
-                Typescript
-              </PanelBlock>
-              <PanelBlock>
-                <Button isOutlined isFullWidth isColor="primary">
-                  {' '}
-                  Analyze
-                </Button>
-              </PanelBlock>
-            </Panel>
+            <Message isColor="light" style={style.marginPanel}>
+              <MessageHeader>Lorem Ipsum</MessageHeader>
+              <MessageBody>
+                <Menu>
+                  <MenuLabel>Metadata</MenuLabel>
+                  <MenuList>
+                    <li>
+                      <MenuLink style={style.removeUnderline}>
+                        <Columns>
+                          <Column isSize="1/3">Name</Column>
+                          <Column isSize="2/3">
+                            {this.state.metadata.name}
+                          </Column>
+                        </Columns>
+                      </MenuLink>
+                    </li>
+                    <li>
+                      <MenuLink style={style.removeUnderline}>
+                        <Columns>
+                          <Column isSize="1/3">Date</Column>
+                          <Column isSize="2/3">
+                            {this.state.metadata.date}
+                          </Column>
+                        </Columns>
+                      </MenuLink>
+                    </li>
+                  </MenuList>
+                  <MenuLabel>Attributes</MenuLabel>
+                  <MenuList>
+                    {this.state.attrib.map(attribute => {
+                      return (
+                        <li>
+                          <MenuLink style={style.removeUnderline}>
+                            <Columns>
+                              <Column isSize="1/3">{attribute.name}</Column>
+                              <Column isSize="2/3">{attribute.value}</Column>
+                            </Columns>
+                          </MenuLink>
+                        </li>
+                      );
+                    })}
+                  </MenuList>
+                </Menu>
+              </MessageBody>
+            </Message>
           </Column>
         </Columns>
       </DocumentTitle>
